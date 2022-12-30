87ea15bb1d2c3d5a35280d6ffa66ac746566ef36
KirillValetko <kirya_valet@mail.ru>
2022-12-30 17:20:23 +0300

    added SkillLevelRepository

--- SkillsGrading.DataAccess/Repositories/SkillGroupRepository.cs
+++ SkillsGrading.DataAccess/Repositories/SkillGroupRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace SkillsGrading.DataAccess.Repositories
+﻿using AutoMapper;
+using SkillsGrading.Common.Helpers.Interfaces;
+using SkillsGrading.DataAccess.DataModels;
+using SkillsGrading.DataAccess.Filters;
+using SkillsGrading.DataAccess.Infrastructure;
+using SkillsGrading.DataAccess.Models;
+using SkillsGrading.DataAccess.Repositories.Interfaces;
+
+namespace SkillsGrading.DataAccess.Repositories
 {
-    public class SkillGroupRepository
+    public class SkillGroupRepository :
+        BaseRepository<SkillGroup, SkillGroupDataModel, SkillGroupFilter>,
+        ISkillGroupRepository
     {
+        public SkillGroupRepository(GradingContext gradingContext,
+            IPaginationHelper<SkillGroup> paginationHelper,
+            IMapper mapper) : base(gradingContext, paginationHelper, mapper)
+        {
+        }
+
+        protected override IQueryable<SkillGroup> AddFilterConditions(IQueryable<SkillGroup> items,
+            SkillGroupFilter filter)
+        {
+            if (!string.IsNullOrEmpty(filter.GroupName))
+            {
+                items = items.Where(x => x.GroupName.Contains(filter.GroupName));
+            }
+            
+            return items;
+        }
     }
 }
---
+++ SkillsGrading.DataAccess/Repositories/SkillLevelRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using SkillsGrading.Common.Helpers.Interfaces;
+using SkillsGrading.DataAccess.DataModels;
+using SkillsGrading.DataAccess.Filters;
+using SkillsGrading.DataAccess.Infrastructure;
+using SkillsGrading.DataAccess.Models;
+using SkillsGrading.DataAccess.Repositories.Interfaces;
+
+namespace SkillsGrading.DataAccess.Repositories
+{
+    public class SkillLevelRepository :
+        BaseRepository<SkillLevel, SkillLevelDataModel, SkillLevelFilter>,
+        ISkillLevelRepository
+    {
+        public SkillLevelRepository(GradingContext gradingContext,
+            IPaginationHelper<SkillLevel> paginationHelper,
+            IMapper mapper) : base(gradingContext, paginationHelper, mapper)
+        {
+        }
+
+        protected override IQueryable<SkillLevel> AddFilterConditions(IQueryable<SkillLevel> items,
+            SkillLevelFilter filter)
+        {
+            if (!string.IsNullOrEmpty(filter.LevelName))
+            {
+                items = items.Where(i => i.LevelName.Contains(filter.LevelName));
+            }
+
+            if (filter.LevelValue.HasValue)
+            {
+                items = items.Where(i => i.LevelValue > filter.LevelValue.Value);
+            }
+
+            if (!string.IsNullOrEmpty(filter.Description))
+            {
+                items = items.Where(i => i.Description.Contains(filter.Description));
+            }
+
+            return items;
+        }
+    }
+}
