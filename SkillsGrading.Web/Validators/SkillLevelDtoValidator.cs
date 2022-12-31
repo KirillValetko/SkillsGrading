54e88d4d2a5c47321aba2b7a0d11c3e327f1f357
KirillValetko <kirya_valet@mail.ru>
2022-12-31 23:30:36 +0300

    added SkillGroupController and validators

---
+++ SkillsGrading.Web/Validators/SkillLevelDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SkillsGrading.Web.Models.DtoModels;
+
+namespace SkillsGrading.Web.Validators
+{
+    public class SkillLevelDtoValidator : AbstractValidator<SkillLevelDto>
+    {
+        public SkillLevelDtoValidator()
+        {
+            RuleFor(sl => sl.LevelName).NotEmpty();
+            RuleFor(sl => sl.LevelValue).GreaterThan(0).NotEmpty();
+            RuleFor(sl => sl.Description).NotEmpty();
+        }
+    }
+}
