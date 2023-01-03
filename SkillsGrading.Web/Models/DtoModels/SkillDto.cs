2593e997823db60441fb7fd11b12556031b1c295
KirillValetko <kirya_valet@mail.ru>
2023-01-03 17:19:33 +0300

    added SkillController

---
+++ SkillsGrading.Web/Models/DtoModels/SkillDto.cs
@@ -0,0 +1,11 @@
+﻿using SkillsGrading.Web.Models.ViewModels;
+
+namespace SkillsGrading.Web.Models.DtoModels
+{
+    public class SkillDto : BaseDto
+    {
+        public string SkillName { get; set; }
+
+        public SkillGroupViewModel SkillGroup { get; set; }
+    }
+}
---
+++ SkillsGrading.Web/Validators/SkillDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SkillsGrading.Web.Models.DtoModels;
+
+namespace SkillsGrading.Web.Validators
+{
+    public class SkillDtoValidator : AbstractValidator<SkillDto>
+    {
+        public SkillDtoValidator()
+        {
+            RuleFor(s => s.SkillName).NotEmpty();
+            RuleFor(s => s.SkillGroup).NotEmpty();
+        }
+    }
+}
