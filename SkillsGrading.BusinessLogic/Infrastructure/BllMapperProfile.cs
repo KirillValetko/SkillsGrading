ee9c0b6950bd4288965b7c0a32ea38f2da564b71
KirillValetko <kirya_valet@mail.ru>
2023-01-14 13:38:56 +0300

    fulfilled mapper profile

--- SkillsGrading.BusinessLogic/Infrastructure/BllMapperProfile.cs
+++ SkillsGrading.BusinessLogic/Infrastructure/BllMapperProfile.cs
@@ -10,9 +10,13 @@ namespace SkillsGrading.BusinessLogic.Infrastructure
         public BllMapperProfile()
         {
             CreateMap<PaginationResponse<SkillGroupDataModel>, PaginationResponse<SkillGroupModel>>();
-            CreateMap<SkillGroupDataModel, SkillGroupModel>().ReverseMap();
+            CreateMap<SkillGroupDataModel, SkillGroupModel>();
+            CreateMap<SkillGroupModel, SkillGroupDataModel>().AfterMap((model, dataModel) =>
+                dataModel.IsActive = true);
             CreateMap<PaginationResponse<SkillLevelDataModel>, PaginationResponse<SkillLevelModel>>();
-            CreateMap<SkillLevelDataModel, SkillLevelModel>().ReverseMap();
+            CreateMap<SkillLevelDataModel, SkillLevelModel>();
+            CreateMap<SkillLevelModel, SkillLevelDataModel>().AfterMap((model, dataModel) => 
+                dataModel.IsActive = true);
         }
     }
 }
