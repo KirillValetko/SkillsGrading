1798624337c6582fc4692a3f9a9097862f193574
KirillValetko <kirya_valet@mail.ru>
2023-02-04 13:19:02 +0300

    added services

--- SkillsGrading.Common/Constants/ExceptionMessageConstants.cs
+++ SkillsGrading.Common/Constants/ExceptionMessageConstants.cs
@@ -4,5 +4,8 @@
     {
         public const string EntityIsNotFound = "Entity is not found";
         public const string EntitiesAreNotFound = "Entities are not found";
+        public const string EqualGroupValue = "There is alredy a group with this group value";
+        public const string DifferentGroupValues = "The group value can\'t be changed";
+        public const string DifferentSpecialties = "The specialty can\'t be changed";
     }
 }
